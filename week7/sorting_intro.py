6f6e44dd1a9a46ab3a98e7e80f5743093ddee423
Rafael A <30427560+rafrafavi@users.noreply.github.com>
2023-08-31 00:19:42 +0800

    Update sorting_intro.py

--- week7/sorting_intro.py
+++ week7/sorting_intro.py
@@ -10,8 +10,6 @@ T = TypeVar('T', int, float)
 def _find_smallest(items: list[T]) -> int:
     smallest_item = items[0]
     smallest_index = 0
-    if x:
-        print("Hello World!")
     for i, item in enumerate(items):
         if item < smallest_item:
             smallest_item = item

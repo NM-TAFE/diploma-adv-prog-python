5ea7b058571e224d452482374dd934f777d1d9f7
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2025-09-02 16:12:10 +0800

    feat: session 7

---
+++ logs/response.py
@@ -0,0 +1,26 @@
+{
+  "coord": { "lon": 115.86, "lat": -31.95 },
+  "weather": [
+    { "id": 800, "main": "Clear", "description": "clear sky", "icon": "01d" }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 20.69,
+    "feels_like": 20.17,
+    "temp_min": 20.69,
+    "temp_max": 20.69,
+    "pressure": 1020,
+    "humidity": 52,
+    "sea_level": 1020,
+    "grnd_level": 1016
+  },
+  "visibility": 10000,
+  "wind": { "speed": 4.58, "deg": 208, "gust": 3.41 },
+  "clouds": { "all": 9 },
+  "dt": 1756798906,
+  "sys": { "country": "AU", "sunrise": 1756765958, "sunset": 1756807229 },
+  "timezone": 28800,
+  "id": 2063523,
+  "name": "Perth",
+  "cod": 200
+}

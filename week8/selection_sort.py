3c898b42cd08f4fd3dda09dc53cebcb187680e04
raf <avigar@markonetechnologies.com.au>
2025-09-09 14:22:26 +0800

    initial selection sort implementation

--- week8/selection_sort.py
+++ week8/selection_sort.py
@@ -20,12 +20,43 @@ def find_smallest_index_and_value(values: Sequence[int]) -> tuple[int, int]:
     return smallest_index, smallest
 
 
+def sort_values(values: list[int]) -> list[int]:
+    """Returns a sorted list"""
+    sorted_list = []
+    while values:
+        index, _ = find_smallest_index_and_value(values)
+        sorted_list.append(values.pop(index))
+    return sorted_list
+
+
+def sort_values_2(values: list[int]) -> None:
+    """Returns a sorted list"""
+    sorted_list = []
+    while values:
+        index, value = find_smallest_index_and_value(values)
+        sorted_list.append(value)
+        values.remove(value)
+    values.extend(sorted_list)
+    return None
+
+
+
+def recursive_sort(values):
+    if len(values) <= 1:
+        return values
+    _, min_val = find_smallest_index_and_value(values)
+    values.remove(min(values))
+    return [min_val] + recursive_sort(values)
+
+
 def test():
     unsorted = create_unsorted_list(100, 5)
     index, smallest = find_smallest_index_and_value(unsorted)
     assert smallest == min(unsorted), f"didn't get the smallest, {unsorted} {smallest} / {min(unsorted)}"
     assert index == unsorted.index(smallest)
     print(f"Smallest in {unsorted} is {smallest} in {index}")
+    the_list = sort_values_2(unsorted)
+    assert sorted(unsorted) == the_list, f"{the_list}" # == recursive_sort(unsorted), f"{recursive_sort(unsorted)}"
 
 
 if __name__ == '__main__':
